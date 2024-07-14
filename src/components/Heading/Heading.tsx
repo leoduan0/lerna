8113ec6af55168c4514c274923aaa81828decdd6
Leo Duan <leoduan0@icloud.com>
2024-07-14 15:48:00 -0700

    Fix Heading component.

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,52 +2,55 @@ import NextLink from 'next/link'
 
 export default function Heading(props: {
   level: 1 | 2 | 3 | 4 | 5
-  children: string
+  children: React.ReactNode
 }): React.ReactNode {
+  const text: string | undefined = props.children?.toString()
+  const link: React.ReactNode = <NextLink href={`#${text}`}>{text}</NextLink>
+
   switch (props.level) {
     case 1:
       return (
         <h1
           className="text-theme text-4xl font-bold transition hover:text-blue-500"
-          id={props.children}
+          id={text}
         >
-          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+          {link}
         </h1>
       )
     case 2:
       return (
         <h2
           className="text-theme text-3xl font-bold transition hover:text-blue-500"
-          id={props.children}
+          id={text}
         >
-          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+          {link}
         </h2>
       )
     case 3:
       return (
         <h3
           className="text-theme text-2xl font-bold transition hover:text-blue-500"
-          id={props.children}
+          id={text}
         >
-          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+          {link}
         </h3>
       )
     case 4:
       return (
         <h4
           className="text-theme text-xl font-bold transition hover:text-blue-500"
-          id={props.children}
+          id={text}
         >
-          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+          {link}
         </h4>
       )
     case 5:
       return (
         <h5
           className="text-theme text-lg font-bold transition hover:text-blue-500"
-          id={props.children}
+          id={text}
         >
-          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+          {link}
         </h5>
       )
   }

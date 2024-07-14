4e769d742f4053b3daff14334d34cea795995ce4
Leo Duan <leoduan0@icloud.com>
2024-07-14 12:22:44 -0700

    Update Heading component.

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,17 +1,54 @@
+import NextLink from 'next/link'
+
 export default function Heading(props: {
   level: 1 | 2 | 3 | 4 | 5
-  children: React.ReactNode
+  children: string
 }): React.ReactNode {
   switch (props.level) {
     case 1:
-      return <h1 className="text-theme text-4xl font-bold">{props.children}</h1>
+      return (
+        <h1
+          className="text-theme text-4xl font-bold transition hover:text-blue-500"
+          id={props.children}
+        >
+          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+        </h1>
+      )
     case 2:
-      return <h2 className="text-theme text-3xl font-bold">{props.children}</h2>
+      return (
+        <h2
+          className="text-theme text-3xl font-bold transition hover:text-blue-500"
+          id={props.children}
+        >
+          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+        </h2>
+      )
     case 3:
-      return <h3 className="text-theme text-2xl font-bold">{props.children}</h3>
+      return (
+        <h3
+          className="text-theme text-2xl font-bold transition hover:text-blue-500"
+          id={props.children}
+        >
+          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+        </h3>
+      )
     case 4:
-      return <h4 className="text-theme text-xl font-bold">{props.children}</h4>
+      return (
+        <h4
+          className="text-theme text-xl font-bold transition hover:text-blue-500"
+          id={props.children}
+        >
+          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+        </h4>
+      )
     case 5:
-      return <h5 className="text-theme text-lg font-bold">{props.children}</h5>
+      return (
+        <h5
+          className="text-theme text-lg font-bold transition hover:text-blue-500"
+          id={props.children}
+        >
+          <NextLink href={`#${props.children}`}>{props.children}</NextLink>
+        </h5>
+      )
   }
 }

494dd0efa8370897310c03bf65fe6702774f595a
Leo Duan <leoduan0@icloud.com>
2024-08-11 10:41:48 -0700

    Minor improvements.

--- src/components/Block/Block.tsx
+++ src/components/Block/Block.tsx
@@ -5,7 +5,7 @@ export default function Block(props: {
   children: React.ReactNode
 }): React.ReactNode {
   return (
-    <div className="w-full space-y-2 rounded-xl bg-neutral-100 p-4 dark:bg-neutral-800">
+    <div className="w-full space-y-2 rounded-xl border border-neutral-200 bg-neutral-100 p-4 dark:border-neutral-600 dark:bg-neutral-800">
       {props.text && (
         <Heading level={5} clickable={false}>
           {props.text}
--- src/components/Problem/ProblemChoices.tsx
+++ src/components/Problem/ProblemChoices.tsx
@@ -18,9 +18,9 @@ export default function ProblemsChoices(props: {
 
   function handleAnswerSubmission(): void {
     if (selected == props.correct - 1) {
-      alert('Correct ✅')
+      alert('Correct')
     } else {
-      alert('Incorrect ❌')
+      alert('Incorrect')
     }
   }
 
@@ -30,7 +30,7 @@ export default function ProblemsChoices(props: {
         {props.choices.map((choice, index) => (
           <li key={index}>
             <button
-              className={`${selected == index ? 'bg-blue-500 dark:bg-blue-600' : 'hover:bg-neutral-300 dark:hover:bg-neutral-500'} w-full cursor-pointer rounded-lg border-2 border-black p-2 text-left transition active:scale-[0.99] dark:border-white`}
+              className={`${selected == index ? 'bg-blue-500 dark:bg-blue-600' : 'hover:bg-neutral-300 dark:hover:bg-neutral-500'} w-full cursor-pointer rounded-lg border border-black p-2 text-left transition active:scale-[0.99] dark:border-white`}
               onClick={() => handleChoiceSelection(index)}
             >
               <span>{choice}</span>
@@ -41,9 +41,9 @@ export default function ProblemsChoices(props: {
       <button
         onClick={() => handleAnswerSubmission()}
         disabled={selected == -1}
-        className="rounded-md p-2 transition active:scale-95 enabled:hover:bg-neutral-200 disabled:cursor-not-allowed enabled:dark:hover:bg-neutral-700"
+        className={`${selected == -1 ? '' : 'active:scale-95'} rounded-md p-2 transition enabled:hover:bg-neutral-200 disabled:cursor-not-allowed enabled:dark:hover:bg-neutral-700`}
       >
-        <span>Submit</span>
+        <span>Check</span>
       </button>
     </div>
   )

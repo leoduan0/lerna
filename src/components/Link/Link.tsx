f9f30522ebef5cea3f4f6d881feb007f5758bfc9
Leo Duan <leoduan0@icloud.com>
2024-08-25 19:03:39 -0700

    Modify Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -5,5 +5,12 @@ interface LinkProps extends React.ComponentProps<typeof NextLink> {
 }
 
 export default function Link({ href, ...props }: LinkProps): React.ReactNode {
-  return <NextLink href={href} {...props} rel="noopener noreferrer" />
+  return (
+    <NextLink
+      href={href}
+      className="text-blue-500 hover:underline"
+      rel="noopener noreferrer"
+      {...props}
+    />
+  )
 }

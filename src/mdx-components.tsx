da63c74c84ab5477963d9b681a76ad818013b1e2
Leo Duan <leoduan0@icloud.com>
2024-08-29 14:47:38 -0700

    Add logo

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -29,9 +29,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {props.children}
       </Link>
     ),
-    Align: Align,
-    Audio: Audio,
-    Block: Block,
     code: (props) => {
       const match = /language-(\w+)/.exec(props.className || '')
       return (
@@ -40,19 +37,22 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         </Code>
       )
     },
-    CommonQuestion: CommonQuestion,
-    Figure: Figure,
-    Graph: Graph,
     h1: (props) => <Heading level={1}>{props.children}</Heading>,
     h2: (props) => <Heading level={2}>{props.children}</Heading>,
     h3: (props) => <Heading level={3}>{props.children}</Heading>,
     h4: (props) => <Heading level={4}>{props.children}</Heading>,
     h5: (props) => <Heading level={5}>{props.children}</Heading>,
+    p: (props) => <p className="text-theme text-base">{props.children}</p>,
+    Align: Align,
+    Audio: Audio,
+    Block: Block,
+    CommonQuestion: CommonQuestion,
+    Figure: Figure,
+    Graph: Graph,
     Image: Image,
     MDX: MDX,
     Math: Math,
     MusicScore: MusicScore,
-    p: (props) => <p className="text-theme text-base">{props.children}</p>,
     Problem: Problem,
     Rule: Rule,
     Showcase: Showcase,

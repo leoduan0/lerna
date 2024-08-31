779255dfca6ca44e940bb13fa8d7bee3f2bab4fb
Leo Duan <leoduan0@icloud.com>
2024-08-30 18:21:02 -0700

    Pagination working

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,11 +6,35 @@ export default function Page() {
       {
         name: "Method 1",
         content: (
-          <>
+          <span>
             Method 1 content
-          </>
+          </span>
         )
-      }
+      },
+      {
+        name: "Method 2",
+        content: (
+          <span>
+            Method 2 content
+          </span>
+        )
+      },
+      {
+        name: "Method 1",
+        content: (
+          <span>
+            Method 1 content
+          </span>
+        )
+      },
+      {
+        name: "Method 1",
+        content: (
+          <span>
+            Method 1 content
+          </span>
+        )
+      },
     ]
   } />
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,23 +1,32 @@
 'use client'
 
+import { Align } from '../Align'
+import { useState } from 'react'
+
 export default function Pagination(props: {
   pages: Array<{ name: string; content: React.ReactNode }>
 }): React.ReactNode {
+  const [selected, setSelected] = useState<number>(0)
+
   return (
-    <div>
+    <Align type={'center'}>
       <div>
-        {props.pages.map((page, index) => {
-          return (
-            <button
-              key={index}
-              id={index.toString()}
-              className="text-theme rounded-md p-1 transition hover:bg-neutral-300 dark:hover:bg-neutral-700"
-            >
-              {page.name}
-            </button>
-          )
-        })}
+        <div className="space-x-2 rounded-md dark:bg-neutral-800">
+          {props.pages.map((page, index) => {
+            return (
+              <button
+                key={index}
+                id={index.toString()}
+                onClick={() => setSelected(index)}
+                className={`${selected == index ? 'bg-neutral-300 font-bold dark:bg-neutral-700' : 'hover:bg-neutral-300 dark:hover:bg-neutral-700'} text-theme rounded-md px-2 py-1 transition active:scale-95`}
+              >
+                {page.name}
+              </button>
+            )
+          })}
+        </div>
+        <div className="bg-purple-500">{props.pages[selected].content}</div>
       </div>
-    </div>
+    </Align>
   )
 }
